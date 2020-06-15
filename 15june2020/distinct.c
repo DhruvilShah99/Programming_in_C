202cb33063ad4a3365879d6a2867a90c8be38e90
DhruvilShah99 <50495300+DhruvilShah99@users.noreply.github.com>
2020-06-15 21:42:55 +0530

    Update distinct.c

--- 15june2020/distinct.c
+++ 15june2020/distinct.c
@@ -1,10 +1,14 @@
+//Write a C program to count distinct elements in an array.
+
 #include <stdio.h>
 void distict_elements(int a[], int n);
 int main()
 {
     int size_array, i, arr[20];
+    printf("Enter Array size \n");
     scanf("%d", &size_array);
-    for(i=0; i<size_array; i++)
+    printf("Enter Array elements \n");
+    for (i = 0; i < size_array; i++)
     {
         scanf("%d", &arr[i]);
     }
@@ -13,15 +17,21 @@ int main()
 }
 void distict_elements(int a[], int n)
 {
+    int count = 0;
     int i, j;
-    for (i=0; i<n; i++)
+    printf("Filered Array : \n");
+    for (i = 0; i < n; i++)
     {
-        for (j=0; j<i; j++)
+        for (j = 0; j < i; j++)
         {
             if (a[i] == a[j])
                 break;
         }
         if (i == j)
-            printf("%d ", a[i]);
+        {
+            count++;
+            printf(" %d ", a[i]);
+        }
     }
+    printf("\n Number of distinct elemets : %d ", count);
 }
